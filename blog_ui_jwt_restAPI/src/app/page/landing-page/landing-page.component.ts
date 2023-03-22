8cd89f299cbd8faf20572e7387b3ed1ef2226182
qiuzhangxi <87635760+qiuzhangxi@users.noreply.github.com>
2023-03-22 15:37:26 +0800

    add eslint + prettier

--- blog_ui_jwt_restAPI/src/app/page/landing-page/landing-page.component.ts
+++ blog_ui_jwt_restAPI/src/app/page/landing-page/landing-page.component.ts
@@ -2,27 +2,24 @@ import { Component, NgModule, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+    selector: 'app-landing-page',
+    templateUrl: './landing-page.component.html',
+    styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
-  constructor (
-  ) { }
+    constructor() {}
 
-  ngOnInit () {
-    console.log('aaa');
-    console.log('bbbb');
-    console.log('aaaaaa');
-  }
+    ngOnInit() {
+        console.log('aaa');
+        console.log('aaaaaa');
+        console.log('aaaaaa');
+    }
 }
 
 @NgModule({
-  imports: [RouterModule],
-  declarations: [
-    LandingPageComponent
-  ],
-  providers: [],
-  exports: [LandingPageComponent]
+    imports: [RouterModule],
+    declarations: [LandingPageComponent],
+    providers: [],
+    exports: [LandingPageComponent],
 })
-export class LandingPageComponentModule { }
+export class LandingPageComponentModule {}
